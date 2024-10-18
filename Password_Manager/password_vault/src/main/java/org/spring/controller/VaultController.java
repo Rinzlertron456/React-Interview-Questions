09cd5f486ca7a2c3ad47651723f29880a2846e71
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2024-10-18 22:02:08 +0530

    push

--- Password_Manager/password_vault/src/main/java/org/spring/controller/VaultController.java
+++ Password_Manager/password_vault/src/main/java/org/spring/controller/VaultController.java
@@ -1,10 +1,25 @@
 package org.spring.controller;
 
+import org.spring.model.Vault;
 import org.spring.repository.VaultRepository;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
 public class VaultController {
+    @Autowired
     private VaultRepository vaultRepo;
-    
+
+    @PostMapping("/vault")
+    Vault vault(@RequestBody Vault vault){
+        return vaultRepo.save(vault);
+    }
+
+    @GetMapping("/vaults")
+    List<Vault> getVaults(){
+        return vaultRepo.findAll();
+    }
 }
--- Password_Manager/password_vault/src/main/java/org/spring/repository/VaultRepository.java
+++ Password_Manager/password_vault/src/main/java/org/spring/repository/VaultRepository.java
@@ -1,8 +1,8 @@
 package org.spring.repository;
 
+import org.spring.model.Vault;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface VaultRepository extends JpaRepository {
+public interface VaultRepository extends JpaRepository<Vault,String> {
 }
